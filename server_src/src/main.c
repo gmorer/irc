eb78c73a43a8c8b785023a02d4e3b7fb94c30456
gmorer <gmorer@student.42.fr>
2019-08-05 11:06:31 +0200

    ipv6 support

--- server_src/src/main.c
+++ server_src/src/main.c
@@ -6,20 +6,34 @@ int error(char *message)
 	return (EXIT_FAILURE);
 }
 
-int init_server()
+int init_server(int isipv6)
 {
-	struct sockaddr_in sockaddr;
-	int sockfd;
+	struct sockaddr_in	sockaddrv4;
+	struct sockaddr_in6 sockaddrv6;
+	int					sockfd;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(isipv6 ? AF_INET6 : AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
 		return (error("Opening socket: "));
-	bzero(&sockaddr, sizeof(struct sockaddr_in));
-	sockaddr.sin_family = AF_INET;
-	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	sockaddr.sin_port = htons(PORT);
-	if (bind(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) == -1)
-		return (error("Binding socket: "));
+	if (isipv6)
+	{
+		bzero(&sockaddrv6, sizeof(struct sockaddr_in6));
+		sockaddrv6.sin6_family = AF_INET6;
+		sockaddrv6.sin6_addr = in6addr_any;
+		sockaddrv6.sin6_port = htons(PORT);
+		if (bind(sockfd, (struct sockaddr *)&sockaddrv6, sizeof(sockaddrv6)) == -1)
+			return (error("Binding socket: "));
+	}
+	else
+	{
+		bzero(&sockaddrv4, sizeof(struct sockaddr_in));
+		sockaddrv4.sin_family = AF_INET;
+		sockaddrv4.sin_addr.s_addr = htonl(INADDR_ANY);
+		sockaddrv4.sin_port = htons(PORT);
+		if (bind(sockfd, (struct sockaddr *)&sockaddrv4, sizeof(sockaddrv4)) == -1)
+			return (error("Binding socket: "));
+	}
+
 	if (listen(sockfd, BACKLOG) == -1)
 		return (error("Listening: "));
 	return (sockfd);
@@ -29,9 +43,19 @@ int main(int argc, char **argv)
 {
 	int			sockfd;
 	t_client	*clients;
+	int			isipv6;
 
 	clients = NULL;
-	if ((sockfd = init_server()) == EXIT_FAILURE)
+	isipv6 = 0;
+	argc -= 1;
+	while (argc)
+	{
+		if (!strcmp(argv[argc], "-ipv6"))
+			isipv6 = 1;
+		argc -= 1;
+	}
+	printf("is ipv6: %d\n", isipv6);
+	if ((sockfd = init_server(isipv6)) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	printf("listening on port %d\n", PORT);
 	server_loop(sockfd, &clients);
